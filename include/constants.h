cf04bed3411938febd3c091b96357d2cbbca9a8b
markoHR01 <marko.marko2adm@gmail.com>
2025-04-06 15:41:15 +0200

    Removed obsolete globals

--- include/constants.h
+++ include/constants.h
@@ -4,18 +4,14 @@
 const int WINDOW_WIDTH = 960;
 const int WINDOW_HEIGHT = 720;
 
-const int TITLE_TOP_X = (WINDOW_WIDTH - 430) / 2;
-const int TITLE_BOTTOM_X = (WINDOW_WIDTH - 350) / 2;
-const int TITLE_TOP_Y = 80;
-const int TITLE_BOTTOM_Y = 190;
+const int TITLE_TOP_X = WINDOW_WIDTH / 2;
+const int TITLE_TOP_Y = 120;
+const int TITLE_BOTTOM_X = WINDOW_WIDTH / 2;
+const int TITLE_BOTTOM_Y = 230;
 const int BUTTON_X = (WINDOW_WIDTH - 220) / 2;
 const int BUTTON_Y = 525;
 const int BUTTON_W = 220;
 const int BUTTON_H = 50;
-const int BUTTON_PLAY_X = BUTTON_X + 82;
-const int BUTTON_PLAY_Y = BUTTON_Y + 8;
-const int BUTTON_QUIT_X = BUTTON_X + 54;
-const int BUTTON_QUIT_Y = BUTTON_Y + 8;
 const int LOAD_ANIMATION_X = (WINDOW_WIDTH - 90) / 2;
 const int LOAD_ANIMATION_Y = BUTTON_Y - 100;
 const int LOAD_SQUARE_SIZE = 10;
