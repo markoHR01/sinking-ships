c9df1629c3c30651e93ee6704ae42a9184aedd86
markoHR01 <marko.marko2adm@gmail.com>
2025-04-12 17:20:31 +0200

    Updated testing enviroment to test setup scene

--- test/test_game.cpp
+++ test/test_game.cpp
@@ -1,16 +1,21 @@
 #include <SDL2/SDL.h>
-#include "game.h"
+#include <SDL2/SDL_ttf.h>
+#include "setup.h"
 #include "constants.h"
 
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
+TTF_Font* font28 = nullptr;
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
 
     window = SDL_CreateWindow("Potapanje Brodova", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                               WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    font28 = TTF_OpenFont("../assets/fonts/OpenSans-ExtraBold.ttf", 28);
 
     SDL_Event e;
     bool running = true;
@@ -19,12 +24,14 @@ int main() {
             if (e.type == SDL_QUIT) running = false;
         }
 
-        renderGameScene(renderer);
+        renderSetupScene(renderer, font28);
         SDL_Delay(16);
     }
 
+    TTF_CloseFont(font28);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_Quit();
     SDL_Quit();
     return 0;
 }
