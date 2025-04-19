232912cefa2b35deffbab8d16a807aeaefc1d522
markoHR01 <marko.marko2adm@gmail.com>
2025-04-19 12:49:11 +0200

    Created/Updated the game scene

--- include/constants.h
+++ include/constants.h
@@ -4,6 +4,8 @@
 const int WINDOW_WIDTH = 960;
 const int WINDOW_HEIGHT = 720;
 
+// Main Menu
+
 const int TITLE_TOP_X = WINDOW_WIDTH / 2;
 const int TITLE_TOP_Y = 120;
 const int TITLE_BOTTOM_X = WINDOW_WIDTH / 2;
@@ -17,6 +19,8 @@ const int LOAD_ANIMATION_Y = BUTTON_Y - 100;
 const int LOAD_SQUARE_SIZE = 10;
 const int LOAD_INTERVAL = 500;
 
+// Setup
+
 const int BOARD_SIZE = 10;
 const int SECTOR_SIZE = 35;
 const int BOARD_SETUP_X = 78;
@@ -26,5 +30,27 @@ const int SHIP_SETUP_Y = 190;
 const int SHIP_SETUP_YADD = 55;
 const int TIMER_SETUP_X = WINDOW_WIDTH / 2;
 const int TIMER_SETUP_Y = 576;
+
+// Game
+
+const int PLAYER_BOARD_X = 50;
+const int PLAYER_BOARD_Y = 50;
+const int ENEMY_BOARD_X = 560;
+const int ENEMY_BOARD_Y = 50;
+const int PLAYER_SHIPS_X = 225;
+const int PLAYER_SHIPS_Y = 450;
+const int ENEMY_SHIPS_X = 735;
+const int ENEMY_SHIPS_Y = 450;
+const int SHIP_GAME_YADD = 45;
+const int TIMER_GAME_X = 480;
+const int TIMER_GAME_Y = 230;
+const int PLAYER_FLEET_X = 480;
+const int PLAYER_FLEET_Y = 130;
+const int ENEMY_FLEET_X = 480;
+const int ENEMY_FLEET_Y = 330;
+const int EXIT_BUTTON_X = 405;
+const int EXIT_BUTTON_Y = 540;
+const int EXIT_BUTTON_W = 150;
+const int EXIT_BUTTON_H = 50;
 
 #endif
--- source/game.cpp
+++ source/game.cpp
@@ -1,4 +1,7 @@
 #include "game.h"
+#include "client.h"
+#include "rendering.h"
+#include "constants.h"
 
 Scene runGameScene(SDL_Renderer* renderer,
                    FontSet& fonts,
@@ -12,6 +15,60 @@ Scene runGameScene(SDL_Renderer* renderer,
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
+
+    Board playerBoard(BOARD_SIZE);
+    Board enemyBoard(BOARD_SIZE);
+
+    renderBoard(renderer, fonts.font28,
+                PLAYER_BOARD_X, PLAYER_BOARD_Y, playerBoard, SECTOR_SIZE);
+    renderBoard(renderer, fonts.font28,
+                ENEMY_BOARD_X, ENEMY_BOARD_Y, enemyBoard, SECTOR_SIZE);
+
+    SDL_Color green = {50, 150, 50, 255};
+
+    renderText(renderer, fonts.font24, "5. Nosač Zrakoplova (5X)",
+               PLAYER_SHIPS_X, PLAYER_SHIPS_Y, green);
+    renderText(renderer, fonts.font24, "4. Bojni Brod (4X)",
+               PLAYER_SHIPS_X, PLAYER_SHIPS_Y + SHIP_GAME_YADD, green);
+    renderText(renderer, fonts.font24, "3. Krstarica (3X)",
+               PLAYER_SHIPS_X, PLAYER_SHIPS_Y + SHIP_GAME_YADD * 2, green);
+    renderText(renderer, fonts.font24, "2. Podmornica (3X)",
+               PLAYER_SHIPS_X, PLAYER_SHIPS_Y + SHIP_GAME_YADD * 3, green);
+    renderText(renderer, fonts.font24, "1. Razarač (2X)",
+               PLAYER_SHIPS_X, PLAYER_SHIPS_Y + SHIP_GAME_YADD * 4, green);
+
+    SDL_Color red = {150, 50, 50, 255};
+
+    renderText(renderer, fonts.font24, "5. Nosač Zrakoplova (5X)",
+               ENEMY_SHIPS_X, ENEMY_SHIPS_Y, red);
+    renderText(renderer, fonts.font24, "4. Bojni Brod (4X)",
+               ENEMY_SHIPS_X, ENEMY_SHIPS_Y + SHIP_GAME_YADD, red);
+    renderText(renderer, fonts.font24, "3. Krstarica (3X)",
+               ENEMY_SHIPS_X, ENEMY_SHIPS_Y + SHIP_GAME_YADD * 2, red);
+    renderText(renderer, fonts.font24, "2. Podmornica (3X)",
+               ENEMY_SHIPS_X, ENEMY_SHIPS_Y + SHIP_GAME_YADD * 3, red);
+    renderText(renderer, fonts.font24, "1. Razarač (2X)",
+               ENEMY_SHIPS_X, ENEMY_SHIPS_Y + SHIP_GAME_YADD * 4, red);
+
+    renderText(renderer, fonts.font28, "(17)",
+               PLAYER_FLEET_X, PLAYER_FLEET_Y, green);
+    renderText(renderer, fonts.font28, "(17)",
+               ENEMY_FLEET_X, ENEMY_FLEET_Y, red);
+
+    renderText(renderer, fonts.font28, "00:15",
+               TIMER_GAME_X, TIMER_GAME_Y, red);
+
+    SDL_Rect exitButton = {EXIT_BUTTON_X, EXIT_BUTTON_Y,
+                           EXIT_BUTTON_W, EXIT_BUTTON_H};
+    SDL_SetRenderDrawColor(renderer, 10, 10, 50, 255);
+    SDL_RenderFillRect(renderer, &exitButton);
+
+    SDL_Color white = {255, 255, 255, 255};
+
+    renderText(renderer, fonts.font24, "Završi",
+               EXIT_BUTTON_X + EXIT_BUTTON_W / 2,
+               EXIT_BUTTON_Y + EXIT_BUTTON_H / 2,
+               white);
 
     SDL_RenderPresent(renderer);
     return Scene::Game;
