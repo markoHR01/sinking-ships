e2d8562c7333ee9d129b21357e66c525978d1f17
markoHR01 <marko.marko2adm@gmail.com>
2025-04-22 21:30:34 +0200

    Moved Board and Ship to separate header files

---
+++ include/board.h
@@ -0,0 +1,35 @@
+#ifdef BOARD_H
+#define BOARD_H
+
+#include <vector>
+
+enum class Token {
+    Empty,
+    Miss,
+    PlayerShip,
+    PlayerShipHit,
+    EnemyShip,
+};
+
+class Board {
+    public:
+        explicit Board(int size)
+                      : board(size * size, Token::Empty),
+                        size_(size) {}
+
+        const Token& operator()(int x, int y) const {
+            return board[y * size_ + x];
+        }
+
+        void operator()(int x, int y, Token token) {
+            board[y * size_ + x] = token;
+        }
+
+        int size() const { return size_; }
+
+    private:
+        std::vector<Token> board;
+        int size_;
+};
+
+#endif
--- include/ship.h
+++ include/ship.h
@@ -1,36 +1,7 @@
-#ifndef CLIENT_H
-#define CLIENT_H
+#ifndef SHIP_H
+#define SHIP_H
 
 #include <vector>
-
-enum class Token {
-    Empty,
-    Miss,
-    PlayerShip,
-    PlayerShipHit,
-    EnemyShip,
-};
-
-class Board {
-    public:
-        explicit Board(int size)
-                      : board(size * size, Token::Empty),
-                        size_(size) {}
-
-        const Token& operator()(int x, int y) const {
-            return board[y * size_ + x];
-        }
-
-        void operator()(int x, int y, Token token) {
-            board[y * size_ + x] = token;
-        }
-
-        int size() const { return size_; }
-
-    private:
-        std::vector<Token> board;
-        int size_;
-};
 
 struct ShipPart {
     const int x, y;
