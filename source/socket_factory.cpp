a1eca39af5200671cf585a268c133986c7a5906c
markoHR01 <marko.marko2adm@gmail.com>
2025-05-11 18:56:54 +0200

    Implemented createSocket()

--- source/socket_factory.cpp
+++ source/socket_factory.cpp
@@ -2,12 +2,28 @@
 
 #ifdef _WIN32
 #include "socket_windows.h"
+#include <winsock2.h>
+
 Socket* createSocket() {
-    return new WindowsSocket();
+    SOCKET socketFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (socketFD == INVALID_SOCKET) {
+        return nullptr;
+    }
+
+    return new WindowsSocket(socketFD);
 }
+
 #else
 #include "socket_unix.h"
+#include <sys/socket.h>
+
 Socket* createSocket() {
-    return new UnixSocket();
+    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD < 0) {
+        return nullptr;
+    }
+
+    return new UnixSocket(socketFD);
 }
+
 #endif
