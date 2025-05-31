16c176d0b01e08b36c07b7b6849d7ca29b124119
markoHR01 <marko.marko2adm@gmail.com>
2025-05-31 17:20:03 +0200

    Configured sockets to enable non-blocking I/O

--- source/socket_factory.cpp
+++ source/socket_factory.cpp
@@ -10,16 +10,30 @@ Socket* createSocket() {
         return nullptr;
     }
 
+    u_long mode = 1;
+    if (ioctlsocket(socketFD, FIONBIO, &mode) != 0) {
+        closesocket(socketFD);
+        return nullptr;
+    }
+
     return new WindowsSocket(socketFD);
 }
 
 #else
 #include "socket_unix.h"
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/socket.h>
 
 Socket* createSocket() {
     int socketFD = socket(AF_INET, SOCK_STREAM, 0);
     if (socketFD < 0) {
+        return nullptr;
+    }
+
+    int flags = fcntl(socketFD, F_GETFL, 0);
+    if (flags == -1 || fcntl(socketFD, F_SETFL, flags | O_NONBLOCK) == -1) {
+        close(socketFD);
         return nullptr;
     }
 
