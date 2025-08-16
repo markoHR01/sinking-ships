c864f80330a99cb4a446cca3c08196eb7cdfa505
markoHR01 <marko.marko2adm@gmail.com>
2025-08-16 11:02:27 +0200

    Added setupStartTime, isPlayerTurn to GameState

--- include/game_state.h
+++ include/game_state.h
@@ -4,11 +4,15 @@
 #include "board.h"
 #include "ship.h"
 #include <vector>
+#include <chrono>
 
 struct GameState {
     bool serverResponsePending;
     bool inQueue;
 
+    std::chrono::steady_clock::time_point setupStartTime;
+
+    bool isPlayerTurn;
     Board* playerBoard;
     Board* enemyBoard;
     std::vector<Ship> playerShips;
